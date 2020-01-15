56e4fbc8bb9dece768cf936e0ee517973c8ab838
Dalius Cepaitis <dalius.cp@gmail.com>
2020-01-15 11:13:48 +0200

    php

---
+++ data/nav.php
@@ -0,0 +1,131 @@
+<?php
+
+//$nav = [
+//    'delfi' => 'Delfi naujienos',
+//    '15min' => '15min naujienos',
+//    'lrytas' => 'Lietuvos ryto naujienos',
+//    'smm' => 'Švietimo ir mokslo ministerija',
+//    'technologijos' => 'Technologijų naujienos'
+//];
+$mokiniai = [
+        'Vardas' => 'Jonas',
+        'Pavardė' => 'Jonaitis',
+        'Klasė' => '5b',
+        'Kodas' => $kodas,
+        'Kontrolinių darbų vidurkis' => $vidurkis,
+        'Duomenų formavimo data' => $data,
+        'Pažymiai' => [
+            "matematika" => $rnd,
+            "geografija" => $rnd,
+            "istorija" => $rnd
+        ]
+
+        'Vardas' => 'Petras',
+        'Pavardė' => 'Petraitis',
+        'Klasė' => '5b',
+        'Kodas' => $kodas,
+        'Kontrolinių darbų vidurkis' => $vidurkis,
+        'Duomenų formavimo data' => $data,
+        'Pažymiai' => [
+            "matematika" => $rnd,
+            "geografija" => $rnd,
+            "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Juozas',
+    'Pavardė' => 'Alijošius',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Danas',
+    'Pavardė' => 'Jotautas',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Joris',
+    'Pavardė' => 'Matulis',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+
+    'Vardas' => 'Benas',
+    'Pavardė' => 'Mintautas',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Gilbertas',
+    'Pavardė' => 'Palionis',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Aleksandras',
+    'Pavardė' => 'Milikevičius',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Arentas',
+    'Pavardė' => 'Balionis',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+
+    'Vardas' => 'Albertas',
+    'Pavardė' => 'Geliulionis',
+    'Klasė' => '5b',
+    'Kodas' => $kodas,
+    'Kontrolinių darbų vidurkis' => $vidurkis,
+    'Duomenų formavimo data' => $data,
+    'Pažymiai' => [
+        "matematika" => $rnd,
+        "geografija" => $rnd,
+        "istorija" => $rnd
+    ]
+];
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include 'config/config.php';
+include 'data/nav.php';
+include 'view/index.view.php';
